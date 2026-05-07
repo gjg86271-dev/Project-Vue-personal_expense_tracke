f1c2336e6c10f5dcb880c41adc278250f9879934
gjg86271-dev <gjg86271@gmail.com>
2026-05-07 14:36:05 +0700

    I update index.js landingpage

---
+++ src/views/LandingView.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+        <nav>
+            <router-link to="/login">Login</router-link>
+            <!-- <router-link to="/register">Register</router-link> -->
+        </nav>
+    </header>
+</template>
