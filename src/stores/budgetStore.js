e01d4ebd6ce22a93b81f54508763c2663de745cf
Phing <cheaphingloem@gmail.com>
2026-05-07 15:53:42 +0700

    Budget-view

--- src/stores/budgetStore.js
+++ src/stores/budgetStore.js
@@ -1,12 +1,28 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import api from '@/api/api' // your axios instance
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useBudgetStore = defineStore('budget', () => {
+  const budgets = ref([])
+  const totalexpenses = ref([])
+  const loading = ref(false)
 
-  return { count, doubleCount, increment }
+  // stores/budget.js
+const fetchBudgets = async () => {
+  const response = await api.get('/budgets?_page=1&_per_page=10')
+
+
+  budgets.value = response.data.data.items
+//   console.log(budgets);
+}
+
+const fetchCategoryBreakdown = async (budgetId) => {
+  const response = await api.get(`analytics/category-breakdown?month=4&year=2026`)
+  totalexpenses.value = response.data.data[0].total
+  console.log(totalexpenses.value);
+}
+
+
+
+  return { budgets, loading, fetchBudgets, fetchCategoryBreakdown, totalexpenses }
 })
